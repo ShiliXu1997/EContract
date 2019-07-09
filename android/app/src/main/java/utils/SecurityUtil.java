229fb23b823dcbdde7c38e7e8518544514c22361
尹伟 <2016137997@qq.com>
2019-07-09 19:53:11 +0800

    内部协调了代码分包

--- android/app/src/main/java/utils/SecurityUtil.java
+++ android/app/src/main/java/utils/SecurityUtil.java
@@ -1,5 +1,7 @@
 package utils;
 
+import android.util.Base64;
+
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.KeyFactory;
@@ -14,7 +16,6 @@ import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
-import android.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,7 +31,7 @@ import javax.crypto.spec.DESKeySpec;
 public class SecurityUtil {
 
     public static String base64Encode(byte[] bytes) {
-        return Base64.encodeToString(bytes,Base64.NO_WRAP);
+        return Base64.encodeToString(bytes, Base64.NO_WRAP);
     }
 
     public static byte[] base64Decode(String str) {
@@ -110,18 +111,8 @@ public class SecurityUtil {
             KeyPair keyPair = keyPairGenerator.generateKeyPair();
             RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
             RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-//            map.put("public_key", base64Encode(publicKey.getEncoded()));
-//            map.put("private_key", base64Encode(privateKey.getEncoded()));
-
-            String temp_pubkey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC0VFArVu0yFuns+jHeHQQ49M+bclVFoHjhBJlMNNQXSyDEA2Gj2BWId1sz+3Sm/kLqFSI45oW6IMf6FRYdEYEz2nQZUWIXRZKwccwcOkDT7X2s7ewYaVnMo5hOBZe/Dr7I7wrfktjwBb4euSpCpSoSF1ReGEMJ3uWAolseRYF+uwIDAQAB";
-            String temp_privkey = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBALRUUCtW7TIW6ez6Md4dBDj0z5tyVUWgeOEEmUw01BdLIMQDYaPYFYh3WzP7dKb+QuoVIjjmhbogx/oVFh0RgTPadBlRYhdFkrBxzBw6QNPtfazt7BhpWcyjmE4Fl78OvsjvCt+S2PAFvh65KkKlKhIXVF4YQwne5YCiWx5FgX67AgMBAAECgYADA0S+MTHQBVogKcyB6rMwSdh/iEke21Ii1EfEz/TaUDQV3bRPjRqVR7wSRnluYjz3PqYlOMsF4Wp4LxPcbpvvwZeJW5TxkgWjuGH2ju7aTRmWJ0reQtxLVfEDZMnKTsPTT6evwIql/YVoly+bJYMC9DTwnoKx91qpb2JTsya6iQJBANn02BZjLFJj8HukH4AVpoGaDpQwNJEGkA5Ry8PaKKJtvKjYV7sWw5l0k+axi8gdujYt1mD69ZGalGXCv0KlXfkCQQDTziR2IMz2zr/juemDPsdtN9ALtGmUBjxHCT2DrkSC9u6AhAAD9EisfY302FRnaEDh0569mrOUGw8Hj3NB0f9TAkASkvQLTF5Nkyi+UKlCkBXFe6x8YnNIXKfQIJZd1WybEwD93pnzXqhCnpWwFjdUUXw5+8QGNbzRsLuuxF7qPg95AkEAuJl/H9HAsg9KIVFkmzX5HFC7q6fnKNMyb/s2uPEG11oeTrY9STT8rhGTyuVM0v+DJQ+K19fgUeIvfDhjjOdY7wJAU3qlqDrA4kUooOT6dxfyhUi4oWg6xNq1cBs01UgjNPGk6BxwQA9G+32rNA/Gl4KZVNo3/o/0757u+CSOygE8Hg==";
-
-            map.put("public_key", temp_pubkey);
-            map.put("private_key", temp_privkey);
-
-            System.out.println("pub:\n"+map.get("public_key"));
-            System.out.println("pri:\n"+map.get("private_key"));
-
+            map.put("public_key", base64Encode(publicKey.getEncoded()));
+            map.put("private_key", base64Encode(privateKey.getEncoded()));
         } catch (NoSuchAlgorithmException noSuchAlgorithmException) {
             noSuchAlgorithmException.printStackTrace();
         }
